422c4af525b8ca64c89551a26a9f68396798729a
Joshua Johnston <mcraftj805@gmail.com>
2025-12-03 20:38:18 -0500

    Update tourism_assignment.R

--- tourism_assignment.R
+++ tourism_assignment.R
@@ -1,13 +1,17 @@
 library(tidyverse)
 library(ggplot2)
 
-
-tourism_data <- read.csv("/home/him/Summer2025/RR_Studio/tourism/API_ST.INT.RCPT.CD_DS2_en_csv_v2_30557.csv", skip = 4) 
+tourism_data <- read.csv("tourism_data.csv", skip = 4) 
 
 # Clean data (only 1995 - 2020) and convert to data long
 tourism_long <- tourism_data %>%
-  pivot_longer(cols = "X1995":"X2020", names_prefix = "X", values_to = "tourism_dollars", names_to = "year")
+  pivot_longer(cols = "X1995":"X2020", names_prefix = "X", values_to = "tourism_dollars", names_to = "year") %>%
+  mutate( # Keep everything as numeric
+    year = as.integer(year),
+    tourism_dollars = as.numeric(tourism_dollars)
+  )
 
+#plotting function
 tourism_plot <- function(..., years = c(1995, 2020)) {
   selected_countries <- c(...)
   
@@ -19,8 +23,9 @@ tourism_plot <- function(..., years = c(1995, 2020)) {
     scale_y_log10() + labs( x = "Year", y = "Dollars")
 }
 
+#examples
 tourism_plot("China", "Ghana", "United States")
-tourism_plot("Aruba", "Gabon", "Serbia", yeras = c(1998, 2013))
+tourism_plot("Aruba", "Gabon", "Serbia", years = c(1998, 2013))
 
   
   
